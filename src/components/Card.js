e17534cb3e224b0c2518e2f9ad2c816bb80ab422
Nicholas Wong <Nickstorm07@gmail.com>
2024-09-08 18:24:32 -0700

    added kawhi not found

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,16 @@ function Card({ manga }) {
       {manga.coverFileName && (
         <img 
           src={`https://uploads.mangadex.org/covers/${manga.id}/${manga.coverFileName}.128.jpg`}
-          alt={'404'}
-       />
+          alt={`Cover for ${manga.attributes.title.en}`}
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = kawhi;
+          }}
+          width = "128"
+          height = "128"
+          style={{ objectFit: 'cover' }}
+
+        />
       )}
       <h3>{manga.attributes.title.en}</h3>
     </div>
